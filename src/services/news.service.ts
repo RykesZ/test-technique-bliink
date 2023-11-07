72a4559d36b05ed8aa707b3e2e56a1980c848d91
rykesz <adfourretout@hotmail.fr>
2023-11-07 22:14:46 +0100

    Set .env.example for future purpose + commented part of the code about env variables being hard coded

--- src/services/news.service.ts
+++ src/services/news.service.ts
@@ -5,6 +5,7 @@ export default class NewsService {
     private apiKey: string | undefined;
     private country: string | undefined;
 
+    // endPoint, apiKey, and country should be in a .env file and processed with REACT_APP env variables system instead of being hard coded, but latest dotenv release is not compatible with webpack > 5 because of polyfills not included anymore ? Fixing this should be a priority
     constructor() {
         this.endPoint = "https://newsapi.org/v2/top-headlines";
         this.apiKey = "f8560a280a574e3fbd2d1c657729870e";
